cf360b5309120f64ad6da7b58d710947c72f79cf
Nicolas Demaria <nicolas.demaria@powtoon.com>
2023-04-02 22:07:58 -0300

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,11 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser()
+  const {data} = api.posts.getAll.useQuery()
+
+  console.log(data)
+
   return (
     <>
       <Head>
@@ -18,6 +20,9 @@ const Home: NextPage = () => {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>{!user.isSignedIn && <SignInButton />}{!!user.isSignedIn && <SignOutButton />}</div>
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+        </div>
       </main>
     </>
   );
