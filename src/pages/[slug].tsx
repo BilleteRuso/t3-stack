3ccfcc23a96b2042cacf891614f3040ef74de817
Nicolas Demaria <nicolas.demaria@powtoon.com>
2023-04-04 01:22:08 -0300

    profile page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import { api } from "~/utils/api";
+import Image from "next/image";
 
 const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
   const { data } = api.profile.getUserByUsername.useQuery({
@@ -16,7 +17,18 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <title>{data.username}</title>
       </Head>
       <PageLayout>
-        {data.username}
+        <div className="relative h-36 bg-slate-600">
+          <Image 
+            src={data.profileImageUrl} 
+            alt={`${data.username ?? ""}'s profile pic`}
+            width={128}
+            height={128}
+            className="-mb-[64px] rounded-full border-4 border-black bg-black absolute bottom-0 left-0 ml-4"
+          />
+        </div>
+        <div className="h-[64px]" />
+        <div className="p-4 text-2xl font-bold">{`@${data.username ?? ""}`}</div>
+        <div className="border-b border-slate-400 w-full" />
       </PageLayout>
     </>
   );
